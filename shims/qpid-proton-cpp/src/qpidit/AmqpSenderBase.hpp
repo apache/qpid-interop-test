59fbdfe572626a01b3178ae6fb7882b5e6bf0690
Kim van der Riet <kpvdr@apache.org>
2016-10-24 20:39:48 -0400

    QPIDIT-43 QPIDIT-44: Minor fix for compiling on RHEL which does not occur on Fedora

--- shims/qpid-proton-cpp/src/qpidit/AmqpSenderBase.hpp
+++ shims/qpid-proton-cpp/src/qpidit/AmqpSenderBase.hpp
@@ -22,6 +22,7 @@
 #ifndef SRC_QPIDIT_AMQPSENDERBASE_HPP_
 #define SRC_QPIDIT_AMQPSENDERBASE_HPP_
 
+#include <stdint.h>
 #include "proton/messaging_handler.hpp"
 #include "qpidit/AmqpTestBase.hpp"
 
